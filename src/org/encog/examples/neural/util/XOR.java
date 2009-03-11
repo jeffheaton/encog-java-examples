6b8daeec25d1cbc14d71951dd1602a6ff40801e4
Jeff Heaton <jeff@jeffheaton.com>
2009-03-11 00:50:26 +0000

    Added generic XOR data class for examples
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@354 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/examples/neural/util/XOR.java
@@ -0,0 +1,9 @@
+package org.encog.examples.neural.util;
+
+public class XOR {
+	
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+		{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+}
