b9b55940ed2e0edf06a2c91a0d95498e632998d7
Jeff Heaton <jeff@jeffheaton.com>
2009-03-12 12:24:24 +0000

    recurrent layers
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@361 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/examples/neural/recurrant/elman/ElmanXOR.java
+++ src/org/encog/examples/neural/recurrant/elman/ElmanXOR.java
@@ -1,16 +1,47 @@
 package org.encog.examples.neural.recurrant.elman;
 
-public class ElmanXOR {
-	
-	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
-		{ 0.0, 1.0 }, { 1.0, 1.0 } };
+import org.encog.examples.neural.util.TemporalXOR;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.layers.ContextLayer;
+import org.encog.neural.networks.layers.Layer;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.backpropagation.Backpropagation;
 
-	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+public class ElmanXOR {
 	
 	
 	
 	public static void main(String args[])
 	{
+		TemporalXOR temp = new TemporalXOR();
+		NeuralDataSet trainingSet = temp.generate(3000);
+		
+		// construct an Elman type network
+		Layer hidden;
+		Layer context = new ContextLayer(2);
+		BasicNetwork network = new BasicNetwork();
+		network.addLayer(new BasicLayer(1));
+		network.addLayer(hidden = new BasicLayer(2));
+		hidden.addNext(context);
+		context.addNext(hidden);
+		network.addLayer(new BasicLayer(1));
+		network.reset();
+		
+		// train the neural network
+		final Train train = new Backpropagation(network, trainingSet,
+				0.0001, 0.1);
+
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			System.out
+					.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while ((epoch < 5000) && (train.getError() > 0.001));
 		
 	}
 }
