4cd0b344b5c2ad43234ef760d7b3f983af8afb6b
Seema Singh <Seemasingh8614@yahoo.com>
2009-09-25 12:20:33 +0000

    More work on Maze example
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@944 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/examples/neural/gui/maze/Constants.java
+++ src/org/encog/examples/neural/gui/maze/Constants.java
@@ -1,5 +1,26 @@
 package org.encog.examples.neural.gui.maze;
 
 public class Constants {
-
+	final public static int VISION_POINTS = 12;
+	final public static int VISION_POINT_12_OCLOCK = 0;
+	final public static int VISION_POINT_1_OCLOCK = 1;
+	final public static int VISION_POINT_2_OCLOCK = 2;
+	final public static int VISION_POINT_3_OCLOCK = 3;
+	final public static int VISION_POINT_4_OCLOCK = 4;
+	final public static int VISION_POINT_5_OCLOCK = 5;
+	final public static int VISION_POINT_6_OCLOCK = 6;
+	final public static int VISION_POINT_7_OCLOCK = 7;
+	final public static int VISION_POINT_8_OCLOCK = 8;
+	final public static int VISION_POINT_9_OCLOCK = 9;
+	final public static int VISION_POINT_10_OCLOCK = 10;
+	final public static int VISION_POINT_11_OCLOCK = 11;
+	
+	final public static int MOTOR_NORTH = 0;
+	final public static int MOTOR_EAST = 1;
+	final public static int MOTOR_SOUTH = 2;
+	final public static int MOTOR_WEST = 3;
+	
+	
+	
+	
 }
--- src/org/encog/examples/neural/gui/maze/NeuralMouse.java
+++ src/org/encog/examples/neural/gui/maze/NeuralMouse.java
@@ -1,30 +1,165 @@
 package org.encog.examples.neural.gui.maze;
 
+import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.basic.BasicNeuralData;
 import org.encog.neural.networks.BasicNetwork;
 
 public class NeuralMouse {
-	
+
 	private final BasicNetwork brain;
 	private final Maze environment;
-	private int row;
-	private int column;
+	private int x;
+	private int y;
+	NeuralData vision;
 
-	public NeuralMouse(BasicNetwork brain, Maze environment)
-	{
+	public NeuralMouse(BasicNetwork brain, Maze environment) {
 		this.brain = brain;
 		this.environment = environment;
-		this.row = 0;
-		this.column = 0;
+		this.x = 0;
+		this.y = 0;
+		this.vision = new BasicNeuralData(Constants.VISION_POINTS);
+	}
+
+	public void updateVision() {
+		// North-East Corner
+		int xNE = x + 1;
+		int yNE = y - 1;
+
+		// North-West Corner
+		int xNW = x - 1;
+		int yNW = y - 1;
+
+		// South-East Corner
+		int xSE = x + 1;
+		int ySE = y + 1;
+
+		// South-West Corner
+		int xSW = x - 1;
+		int ySW = y + 1;
+
+		// twelve o'clock
+		this.vision.setData(Constants.VISION_POINT_12_OCLOCK, environment
+				.isWall(x, y, Maze.NORTH) ? 1 : -1);
+
+		// one o'clock
+		this.vision.setData(Constants.VISION_POINT_1_OCLOCK, environment
+				.isWall(xNE, yNE, Maze.WEST) ? 1 : -1);
+
+		// two o'clock
+		this.vision.setData(Constants.VISION_POINT_2_OCLOCK, environment
+				.isWall(xNE, yNE, Maze.SOUTH) ? 1 : -1);
+
+		// three o'clock
+		this.vision.setData(Constants.VISION_POINT_3_OCLOCK, environment
+				.isWall(x, y, Maze.EAST) ? 1 : -1);
+
+		// four o'clock
+		this.vision.setData(Constants.VISION_POINT_4_OCLOCK, environment
+				.isWall(xSE, ySE, Maze.NORTH) ? 1 : -1);
+
+		// five o'clock
+		this.vision.setData(Constants.VISION_POINT_5_OCLOCK, environment
+				.isWall(xSE, ySE, Maze.WEST) ? 1 : -1);
+
+		// six o'clock
+		this.vision.setData(Constants.VISION_POINT_6_OCLOCK, environment
+				.isWall(x, y, Maze.SOUTH) ? 1 : -1);
+
+		// seven o'clock
+		this.vision.setData(Constants.VISION_POINT_7_OCLOCK, environment
+				.isWall(xSW, ySW, Maze.EAST) ? 1 : -1);
+
+		// eight o'clock
+		this.vision.setData(Constants.VISION_POINT_8_OCLOCK, environment
+				.isWall(xSW, ySW, Maze.NORTH) ? 1 : -1);
+
+		// nine o'clock
+		this.vision.setData(Constants.VISION_POINT_9_OCLOCK, environment
+				.isWall(x, y, Maze.WEST) ? 1 : -1);
+
+		// ten o'clock
+		this.vision.setData(Constants.VISION_POINT_10_OCLOCK, environment
+				.isWall(xNW, yNW, Maze.SOUTH) ? 1 : -1);
+
+		// eleven o'clock
+		this.vision.setData(Constants.VISION_POINT_11_OCLOCK, environment
+				.isWall(xNW, yNW, Maze.EAST) ? 1 : -1);
+	}
+
+	public boolean move(int direction) {
+		
+		if( this.environment.isWall(this.x, this.y, direction))
+			return false;
+
+		
+		switch (direction) {
+		case Maze.NORTH:
+			y--;
+			break;
+		case Maze.SOUTH:
+			y++;
+			break;
+		case Maze.EAST:
+			x++;
+			break;
+		case Maze.WEST:
+			x--;
+			break;
+		}
+
+		return true;
 	}
 	
-	public int getRow()
+	public int autonomousMoveDirection()
 	{
-		return this.row;
+		updateVision();
+		NeuralData result = this.brain.compute(this.vision);
+		
+		double winningOutput = Double.NEGATIVE_INFINITY;
+		int winningIndex = -1;
+		
+		for(int i=0;i<result.size();i++)
+		{
+			double thisOutput = result.getData(i);
+			if( thisOutput>winningOutput)
+			{
+				winningOutput = thisOutput;
+				winningIndex = i;
+			}
+		}
+		
+		if( winningIndex==Constants.MOTOR_NORTH )
+			return Maze.NORTH;
+		else if( winningIndex==Constants.MOTOR_SOUTH )
+			return Maze.SOUTH;
+		else if( winningIndex==Constants.MOTOR_EAST )
+			return Maze.EAST;
+		else
+			return Maze.WEST;
+		
 	}
 	
-	public int getColumn()
+	public boolean move()
 	{
-		return this.column;
+		int direction = autonomousMoveDirection();
+		return false;
+		
 	}
-	
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 }
