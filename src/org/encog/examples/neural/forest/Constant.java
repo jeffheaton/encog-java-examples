eac03551ce9e5aeb3da3de8086349e8e1cd35190
Seema Singh <Seemasingh8614@yahoo.com>
2009-10-21 10:54:59 +0000

    More enhancement to normalization
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@1039 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/examples/neural/forest/Constant.java
+++ src/org/encog/examples/neural/forest/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
 	public static final File BASE_DIRECTORY = new File("c:\\data");
 	public static final File COVER_TYPE_FILE = new File(BASE_DIRECTORY,"covtype.data");
 	public static final File TRAINING_FILE = new File(BASE_DIRECTORY,"training.bin");
-	public static final File EVAL_FILE = new File(BASE_DIRECTORY,"eval.bin");
+	public static final File EVAL_FILE = new File(BASE_DIRECTORY,"eval.csv");
 	public static final File TRAINED_NETWORK_FILE = new File(BASE_DIRECTORY,"forest.eg");
 	
 	public static final String TRAINED_NETWORK_NAME = "forest-network";
