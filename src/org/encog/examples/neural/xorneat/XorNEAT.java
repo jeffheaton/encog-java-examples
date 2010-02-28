4a02e8df705f6c0d6ad72096330c8d70f6e30bb6
Jeff Heaton <jeff@jeffheaton.com>
2010-02-28 11:57:06 +0000

    Added NEAT example
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@1307 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/examples/neural/xorneat/XorNEAT.java
@@ -0,0 +1,93 @@
+/*
+ * Encog(tm) Artificial Intelligence Framework v2.3
+ * Java Examples
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
+ */
+
+package org.encog.examples.neural.xorneat;
+
+import org.encog.math.randomize.FanInRandomizer;
+import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.NeuralDataPair;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.CalculateScore;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.TrainingSetScore;
+import org.encog.neural.networks.training.genetic.NeuralGeneticAlgorithm;
+import org.encog.util.logging.Logging;
+import org.encog.util.simple.EncogUtility;
+import org.encog.neural.networks.training.neat.NEATTraining;
+
+/**
+ * XOR-Genetic: This example solves the classic XOR operator neural
+ * network problem.  However, it uses a genetic algorithm, rather than
+ * backpropagation.
+ * 
+ * @author $Author$
+ * @version $Revision$
+ */
+public class XorNEAT {
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+	public static void main(final String args[]) {
+		
+		Logging.stopConsoleLogging();
+
+		NeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT, XOR_IDEAL);
+		
+		CalculateScore score = new TrainingSetScore(trainingSet);
+		// train the neural network
+		final Train train = new NEATTraining(
+				score, 2, 1, 1000);
+
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			System.out
+					.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while ((epoch < 5000) && (train.getError() > 0.001));
+
+		BasicNetwork network = train.getNetwork();
+
+		// test the neural network
+		System.out.println("Neural Network Results:");
+		for(NeuralDataPair pair: trainingSet ) {
+			final NeuralData output = network.compute(pair.getInput());
+			System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+					+ ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+		}
+	}
+}
