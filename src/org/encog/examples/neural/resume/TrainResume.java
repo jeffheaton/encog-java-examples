c9cfae8de00f2d26e87dd319202cbc1f9ddae559
Seema Singh <Seemasingh8614@yahoo.com>
2010-08-19 18:15:10 +0000

    Added training continue example.
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@1855 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/examples/neural/resume/TrainResume.java
@@ -0,0 +1,54 @@
+package org.encog.examples.neural.resume;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.propagation.TrainingContinuation;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.neural.networks.training.strategy.RequiredImprovementStrategy;
+import org.encog.util.SerializeObject;
+import org.encog.util.logging.Logging;
+import org.encog.util.simple.EncogUtility;
+
+public class TrainResume {
+	
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+		{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+	
+	public static void main(String[] args)
+	{
+		Logging.stopConsoleLogging();
+		NeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT, XOR_IDEAL);
+		BasicNetwork network = EncogUtility.simpleFeedForward(2, 4, 0, 1, false);
+		ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+		train.addStrategy(new RequiredImprovementStrategy(5));
+		
+		System.out.println("Perform initial train.");
+		EncogUtility.trainToError(train,network, trainingSet, 0.01);
+		TrainingContinuation cont = train.pause();
+		System.out.println(Arrays.toString((double[])cont.getContents().get(ResilientPropagation.LAST_GRADIENTS)));
+		System.out.println(Arrays.toString((double[])cont.getContents().get(ResilientPropagation.UPDATE_VALUES)));
+		
+		try
+		{
+		SerializeObject.save("resume.ser", cont);
+		cont = (TrainingContinuation)SerializeObject.load("resume.ser");
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		System.out.println("Now trying a second train, with continue from the first.  Should stop after one iteration");
+		ResilientPropagation train2 = new ResilientPropagation(network, trainingSet);
+		train2.resume(cont);
+		EncogUtility.trainToError(train2,network, trainingSet, 0.01);	
+	}
+}
