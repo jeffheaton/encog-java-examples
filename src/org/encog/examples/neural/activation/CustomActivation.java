b0ea50a40939c6aa188682268152510dc7dde2c1
Seema Singh <Seemasingh8614@yahoo.com>
2010-09-29 18:08:15 +0000

    Example of using a custom activation function
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@2131 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/org/encog/examples/neural/activation/CustomActivation.java
@@ -0,0 +1,88 @@
+/*
+ * Encog(tm) Examples v2.4
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
+ */
+
+package org.encog.examples.neural.activation;
+
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.neural.data.NeuralData;
+import org.encog.neural.data.NeuralDataPair;
+import org.encog.neural.data.NeuralDataSet;
+import org.encog.neural.data.basic.BasicNeuralDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.neural.networks.training.strategy.RequiredImprovementStrategy;
+import org.encog.util.logging.Logging;
+
+
+public class CustomActivation {
+
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+	public static void main(final String args[]) {
+		
+		Logging.stopConsoleLogging();
+		
+		BasicNetwork network = new BasicNetwork();
+		network.addLayer(new BasicLayer(null, true,2));
+		network.addLayer(new BasicLayer(new ActivationSigmoidPosNeg(), true,4));
+		network.addLayer(new BasicLayer(new ActivationSigmoidPosNeg(), true,1));
+		network.getStructure().finalizeStructure();
+		network.reset();
+
+		NeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT, XOR_IDEAL);
+		
+		// train the neural network
+		final Train train = new ResilientPropagation(network, trainingSet);
+		// reset if improve is less than 1% over 5 cycles
+		train.addStrategy(new RequiredImprovementStrategy(5));
+		
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			System.out
+					.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while(train.getError() > 0.01);
+
+		// test the neural network
+		System.out.println("Neural Network Results:");
+		for(NeuralDataPair pair: trainingSet ) {
+			final NeuralData output = network.compute(pair.getInput());
+			System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+					+ ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+		}
+	}
+}
