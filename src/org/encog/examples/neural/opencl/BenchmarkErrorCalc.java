0151af7a47be4a9632958196122e9a0f3d888e49
Jeff Heaton <jeff@jeffheaton.com>
2010-11-12 17:19:51 +0000

    Added to benchmark
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@2291 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/examples/neural/opencl/BenchmarkErrorCalc.java
+++ src/org/encog/examples/neural/opencl/BenchmarkErrorCalc.java
@@ -4,6 +4,7 @@ import org.encog.Encog;
 import org.encog.engine.concurrency.calc.ConcurrentCalculate;
 import org.encog.engine.data.EngineIndexableSet;
 import org.encog.engine.network.flat.FlatNetwork;
+import org.encog.engine.util.Stopwatch;
 import org.encog.neural.data.NeuralDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.util.benchmark.RandomTrainingFactory;
@@ -11,11 +12,11 @@ import org.encog.util.simple.EncogUtility;
 
 public class BenchmarkErrorCalc {
 	
-	public static final int TRAINING_SIZE = 1000;
-	public static final int INPUT_SIZE = 10;
-	public static final int IDEAL_SIZE = 10;
-	public static final int HIDDEN1_SIZE = 50;
-	public static final int HIDDEN2_SIZE = 50;
+	public static final int TRAINING_SIZE = 100000;
+	public static final int INPUT_SIZE = 300;
+	public static final int IDEAL_SIZE = 1;
+	public static final int HIDDEN1_SIZE = 300;
+	public static final int HIDDEN2_SIZE = 300;
 	
 	
 	public static void main(String[] args)
@@ -32,13 +33,20 @@ public class BenchmarkErrorCalc {
 		
 		calc.setTrainingData(training);
 		calc.setNetwork(flat);
-		double e1 = calc.calculateError();
 		
+		Stopwatch sw1 = new Stopwatch();
+		sw1.start();
+		double e1 = calc.calculateError();
+		sw1.stop();
+		
+		Stopwatch sw2 = new Stopwatch();
 		Encog.getInstance().initCL();
 		calc.initCL();
+		sw2.start();
 		double e2 = calc.calculateError();
+		sw2.stop();
 		
-		System.out.println("CPU-Only Error:" + e1);
-		System.out.println("CPU&GPU Error:" + e2);
+		System.out.println("CPU-Only Error:" + e1 + ",time=" + sw1.getElapsedMilliseconds());
+		System.out.println("CPU&GPU Error:" + e2  + ",time=" + sw2.getElapsedMilliseconds());
 	}
 }
