fb52e50a0a340328665d4904db60e693deaa0bd1
Seema Singh <Seemasingh8614@yahoo.com>
2010-12-10 19:09:47 +0000

    More PNN enhancements
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@2368 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/examples/neural/xorpnn/XorPNN.java
+++ src/org/encog/examples/neural/xorpnn/XorPNN.java
@@ -11,38 +11,76 @@ import org.encog.neural.pnn.PNNKernelType;
 import org.encog.neural.pnn.PNNOutputMode;
 import org.encog.util.logging.Logging;
 
-
 public class XorPNN {
+
 	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
-		{ 0.0, 1.0 }, { 1.0, 1.0 } };
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
 
-public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
 
-public static void main(final String args[]) {
 	
-	Logging.stopConsoleLogging();
-	PNNOutputMode mode = PNNOutputMode.Regression;
+	public static void evaluate( BasicPNN network, BasicNeuralDataSet trainingSet)
+	{
+		for (final NeuralDataPair pair : trainingSet) {
+			final NeuralData output = network.compute(pair.getInput());
+			
+			Arrays.toString(output.getData());
+			System.out.print("Input: ");
+			System.out.print(Arrays.toString(pair.getInput().getData()));
+			
+			if( pair.isSupervised() ) {
+				System.out.print(", Ideal: ");
+				System.out.print(Arrays.toString(pair.getIdeal().getData()));
+			}
+			
+			System.out.print(", Actual: ");
+			System.out.println(Arrays.toString(output.getData()));
+		}		
+	}
 	
-	BasicPNN network = new BasicPNN(
-			PNNKernelType.Gaussian, 
-			mode, 
-			2,
-			1);
-	
-	BasicNeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT, XOR_IDEAL);
-	
+	public static void regressionExample()
+	{
+		System.out.println();
+		System.out.println("** Running in regression mode **");
+		
+		PNNOutputMode mode = PNNOutputMode.Regression;
 
-	System.out.println("Learning...");
+		BasicPNN network = new BasicPNN(PNNKernelType.Gaussian, mode, 2, 1);
+
+		BasicNeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT,
+				XOR_IDEAL);
+
+		System.out.println("Learning...");
+
+		TrainBasicPNN train = new TrainBasicPNN(network, trainingSet);
+		train.learn();
+		evaluate(network,trainingSet);
+	}
 	
-	TrainBasicPNN train = new TrainBasicPNN(network,trainingSet);
-	train.learn();
+	public static void classificationExample()
+	{
+		System.out.println();
+		System.out.println("** Running in classification mode **");
+		
+		PNNOutputMode mode = PNNOutputMode.Classification;
+
+		BasicPNN network = new BasicPNN(PNNKernelType.Gaussian, mode, 2, 2);
+
+		BasicNeuralDataSet trainingSet = new BasicNeuralDataSet(XOR_INPUT,
+				XOR_IDEAL);
+
+		System.out.println("Learning...");
+
+		TrainBasicPNN train = new TrainBasicPNN(network, trainingSet);
+		train.learn();
+		evaluate(network,trainingSet);
+	}
 	
-	for (final NeuralDataPair pair : trainingSet) {
-		final NeuralData output = network.compute(pair.getInput());
-		System.out.println(pair.getInput().getData(0) + ","
-				+ pair.getInput().getData(1) + ", actual="
-				+ output.getData(0) + ",ideal="
-				+ pair.getIdeal().getData(0));
-	}	
+	
+	public static void main(final String args[]) {
+
+		Logging.stopConsoleLogging();
+		regressionExample();
+		classificationExample();
 	}
 }
