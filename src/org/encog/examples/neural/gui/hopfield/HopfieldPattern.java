b02d8e5c7f02b5135ecc926f69db4f85bf83db6b
Seema Singh <Seemasingh8614@yahoo.com>
2011-03-16 14:23:27 +0000

    Code cleanup on new Hopfield GUI example
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@2829 f90f6e9a-ac51-0410-b353-d1b83c6f6923

--- src/org/encog/examples/neural/gui/hopfield/HopfieldPattern.java
+++ src/org/encog/examples/neural/gui/hopfield/HopfieldPattern.java
@@ -1,6 +1,7 @@
 package org.encog.examples.neural.gui.hopfield;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -20,8 +21,10 @@ import org.encog.neural.networks.training.propagation.resilient.ResilientPropaga
 import org.encog.neural.thermal.HopfieldNetwork;
 import org.encog.util.simple.EncogUtility;
 
-public class HopfieldPattern extends JFrame  implements ActionListener, MouseListener {
+public class HopfieldPattern extends JFrame  implements ActionListener {
 
+	private HopfieldPanel panel;
+	
 	public HopfieldPattern()
 	{
 		this.setTitle("Hopfield Pattern");
@@ -29,41 +32,27 @@ public class HopfieldPattern extends JFrame  implements ActionListener, MouseLis
 		Container content = this.getContentPane();
 		content.setLayout(new BorderLayout());
 		
-		this.grid = new boolean[this.GRID_X * this.GRID_Y];
-		this.addMouseListener(this);
 		this.buttonTrain = new JButton("Train");
 		this.buttonGo = new JButton("Go");
 		this.buttonClear = new JButton("Clear");
-		this.buttonClearMatrix = new JButton("Clear Matrix");
-		this.setLayout(new BorderLayout());
+		this.buttonClearMatrix = new JButton("Clear Matrix");		
 		this.buttonPanel = new JPanel();
 		this.buttonPanel.add(this.buttonTrain);
 		this.buttonPanel.add(this.buttonGo);
 		this.buttonPanel.add(this.buttonClear);
 		this.buttonPanel.add(this.buttonClearMatrix);
-		this.add(this.buttonPanel, BorderLayout.SOUTH);
+		content.add(this.buttonPanel, BorderLayout.SOUTH);
 
 		this.buttonTrain.addActionListener(this);
 		this.buttonGo.addActionListener(this);
 		this.buttonClear.addActionListener(this);
 		this.buttonClearMatrix.addActionListener(this);
-
-		this.hopfield = new HopfieldNetwork(this.GRID_X * this.GRID_Y);
-
 		
+		this.panel = new HopfieldPanel(20,20);
+		content.add(this.panel, BorderLayout.CENTER);
 	}
 	
-	/**
-	 * Serial id for this class.
-	 */
-	private static final long serialVersionUID = 1882626251007826502L;
-	public final int GRID_X = 8;
-	public final int GRID_Y = 8;
-	public final int CELL_WIDTH = 20;
-	public final int CELL_HEIGHT = 20;
-	public HopfieldNetwork hopfield;
-	private boolean grid[];
-	private int margin;
+
 	private JPanel buttonPanel;
 	private JButton buttonTrain;
 	private JButton buttonGo;
@@ -72,121 +61,16 @@ public class HopfieldPattern extends JFrame  implements ActionListener, MouseLis
 
 	public void actionPerformed(final ActionEvent e) {
 		if (e.getSource() == this.buttonClear) {
-			clear();
+			this.panel.clear();
 		} else if (e.getSource() == this.buttonClearMatrix) {
-			clearMatrix();
+			this.panel.clearMatrix();
 		} else if (e.getSource() == this.buttonGo) {
-			go();
+			this.panel.go();
 		} else if (e.getSource() == this.buttonTrain) {
-			train();
+			this.panel.train();
 		}
 	}
 
-	/**
-	 * Clear the grid.
-	 */
-	public void clear() {
-		int index = 0;
-		for (int y = 0; y < this.GRID_Y; y++) {
-			for (int x = 0; x < this.GRID_X; x++) {
-				this.grid[index++] = false;
-			}
-		}
-
-		repaint();
-	}
-
-	/**
-	 * Clear the weight matrix.
-	 */
-	private void clearMatrix() {
-		EngineArray.fill(this.hopfield.getWeights(),0);
-	}
-
-	/**
-	 * Run the neural network.
-	 */
-	public void go() {
-		for(int i=0;i<this.grid.length;i++) {
-			this.hopfield.getCurrentState().setData(i, grid[i]);	
-		}
-		
-		this.hopfield.run();
-		
-		for(int i=0;i<this.grid.length;i++) {
-			grid[i] = this.hopfield.getCurrentState().getBoolean(i);	
-		}
-		repaint();
-
-	}
-
-	public void mouseReleased(final MouseEvent e) {
-		final int x = ((e.getX() - this.margin) / this.CELL_WIDTH);
-		final int y = e.getY() / this.CELL_HEIGHT;
-		if (((x >= 0) && (x < this.GRID_X)) && ((y >= 0) && (y < this.GRID_Y))) {
-			final int index = (y * this.GRID_X) + x;
-			this.grid[index] = !this.grid[index];
-		}
-		repaint();
-
-	}
-
-	@Override
-	public void paint(final Graphics g) {
-		this.margin = (this.getWidth() - (this.CELL_WIDTH * this.GRID_X)) / 2;
-		int index = 0;
-		for (int y = 0; y < this.GRID_Y; y++) {
-			for (int x = 0; x < this.GRID_X; x++) {
-				if (this.grid[index++]) {
-					g.fillRect(this.margin + (x * this.CELL_WIDTH), y
-							* this.CELL_HEIGHT, this.CELL_WIDTH,
-							this.CELL_HEIGHT);
-				} else {
-					g.drawRect(this.margin + (x * this.CELL_WIDTH), y
-							* this.CELL_HEIGHT, this.CELL_WIDTH,
-							this.CELL_HEIGHT);
-				}
-			}
-		}
-	}
-
-	/**
-	 * Train the neural network.
-	 */
-	public void train() {
-		BiPolarNeuralData pattern = new BiPolarNeuralData(this.grid.length);
-		
-		for(int i=0;i<this.grid.length;i++) {
-			pattern.setData(i, grid[i]);	
-		}
-
-		this.hopfield.addPattern(pattern);
-
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
 
 	public static void main(String[] args)
