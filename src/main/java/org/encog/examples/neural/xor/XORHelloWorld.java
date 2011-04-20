36ffba15370b7f84572100566c690ff88ed6f694
Seema Singh <Seemasingh8614@yahoo.com>
2011-04-20 15:16:50 +0000

    beginning to re-org xor examples
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@3274 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/main/java/org/encog/examples/neural/xor/XORHelloWorld.java
@@ -0,0 +1,89 @@
+/*
+ * Encog(tm) Examples v2.6 
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ 
+ * Copyright 2008-2010 Heaton Research, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *   
+ * For more information on Heaton Research copyrights, licenses 
+ * and trademarks visit:
+ * http://www.heatonresearch.com/copyright
+ */
+package org.encog.examples.neural.xor;
+
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.ml.train.MLTrain;
+import org.encog.neural.activation.ActivationSigmoid;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.back.Backpropagation;
+import org.encog.util.logging.Logging;
+
+/**
+ * XOR: This example is essentially the "Hello World" of neural network
+ * programming.  This example shows how to construct an Encog neural
+ * network to predict the output from the XOR operator.  This example
+ * uses backpropagation to train the neural network.
+ * 
+ * @author $Author$
+ * @version $Revision$
+ */
+public class XORHelloWorld {
+
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+	public static void main(final String args[]) {
+		
+		// stop console logging
+		Logging.stopConsoleLogging();
+		
+		// create a neural network, without using a factory
+		BasicNetwork network = new BasicNetwork();
+		network.addLayer(new BasicLayer(null,false,2));
+		network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
+		network.addLayer(new BasicLayer(new ActivationSigmoid(),true,1));
+		network.getStructure().finalizeStructure();
+		network.reset();
+
+		// create training data
+		MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
+		
+		// train the neural network
+		final MLTrain train = new Backpropagation(network, trainingSet, 0.7, 0.8);
+
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			System.out
+					.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while(train.getError() > 0.01);
+
+		// test the neural network
+		System.out.println("Neural Network Results:");
+		for(MLDataPair pair: trainingSet ) {
+			final MLData output = network.compute(pair.getInput());
+			System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+					+ ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+		}
+	}
+}
