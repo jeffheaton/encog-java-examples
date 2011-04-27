9a5b868b71c26a85acc41e75dc63a289eeb5b4a0
Seema Singh <Seemasingh8614@yahoo.com>
2011-04-27 13:49:21 +0000

    Added XOR flat example
    git-svn-id: http://encog-java.googlecode.com/svn/trunk/encog-examples@3348 f90f6e9a-ac51-0410-b353-d1b83c6f6923

---
+++ src/main/java/org/encog/examples/neural/xor/XORFlat.java
@@ -0,0 +1,86 @@
+/*
+ * Encog(tm) Examples v2.4
+ * http://www.heatonresearch.com/encog/
+ * http://code.google.com/p/encog-java/
+ * 
+ * Copyright 2008-2010 by Heaton Research Inc.
+ * 
+ * Released under the LGPL.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+ * For information on Heaton Research trademarks, visit:
+ * 
+ * http://www.heatonresearch.com/copyright.html
+ */
+
+package org.encog.examples.neural.xor;
+
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.flat.FlatNetwork;
+import org.encog.neural.flat.train.prop.TrainFlatNetworkResilient;
+import org.encog.util.logging.Logging;
+
+/**
+ * XOR: This example is essentially the "Hello World" of neural network
+ * programming.  This example shows how to construct an Encog neural
+ * network to predict the output from the XOR operator.  This example uses
+ * a flat neural network.
+ */
+public class XORFlat {
+
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+
+	public static void main(final String args[]) {
+		
+		Logging.stopConsoleLogging();
+		
+		FlatNetwork network = new FlatNetwork(2,4,0,1,false);
+		network.randomize();
+		
+		MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
+		
+		
+		TrainFlatNetworkResilient train = new TrainFlatNetworkResilient(network,trainingSet);
+		
+		//Encog.getInstance().initCL();
+		//train.setTargetDevice(Encog.getInstance().getCL().getDevices().get(0));
+		
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			System.out
+					.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while(train.getError() > 0.01 );
+
+		double[] output = new double[1];
+		// test the neural network
+		System.out.println("Neural Network Results:");
+		for(MLDataPair pair: trainingSet ) {
+			double[] input = pair.getInput().getData();
+			network.compute(input, output);
+			System.out.println(input[0] + "," + input[1] + ":" + output[0]);
+		}
+	}
+}
