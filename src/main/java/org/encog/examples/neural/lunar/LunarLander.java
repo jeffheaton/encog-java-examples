da0f35ee62e85f92619324b7227d6cdd5a1b7f44
Seema Singh <Seemasingh8614@yahoo.com>
2011-09-29 11:39:33 -0500

    Fixed lunar lander to use NguyenWidrowRandomizer

--- src/main/java/org/encog/examples/neural/lunar/LunarLander.java
+++ src/main/java/org/encog/examples/neural/lunar/LunarLander.java
@@ -25,7 +25,7 @@ package org.encog.examples.neural.lunar;
 
 import org.encog.Encog;
 import org.encog.engine.network.activation.ActivationTANH;
-import org.encog.mathutil.randomize.FanInRandomizer;
+import org.encog.mathutil.randomize.NguyenWidrowRandomizer;
 import org.encog.ml.train.MLTrain;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.training.anneal.NeuralSimulatedAnnealing;
@@ -69,7 +69,7 @@ public class LunarLander {
 		else
 		{
 			train = new NeuralGeneticAlgorithm(
-					network, new FanInRandomizer(), 
+					network, new NguyenWidrowRandomizer(), 
 					new PilotScore(),500, 0.1, 0.25);
 		}
 		
