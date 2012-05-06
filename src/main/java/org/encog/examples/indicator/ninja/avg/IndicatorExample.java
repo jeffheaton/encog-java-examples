5429ca834817abf219bd7f508283302521541078
Jeff Heaton <jeff@jeffheaton.com>
2012-05-06 16:37:46 -0500

    Work on indicator example

---
+++ src/main/java/org/encog/examples/indicator/ninja/avg/IndicatorExample.java
@@ -0,0 +1,103 @@
+package org.encog.examples.indicator.ninja.avg;
+
+import java.io.File;
+
+import org.encog.cloud.indicator.IndicatorConnectionListener;
+import org.encog.cloud.indicator.IndicatorFactory;
+import org.encog.cloud.indicator.IndicatorListener;
+import org.encog.cloud.indicator.server.IndicatorLink;
+import org.encog.cloud.indicator.server.IndicatorServer;
+
+public class IndicatorExample implements IndicatorConnectionListener {
+
+	public static final int PORT = 5128;
+	private File path;
+
+	public IndicatorExample(File thePath) {
+		this.path = thePath;
+	}
+
+	public void train() {
+
+	}
+
+	private void generate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void clear() {
+		File[] list = this.path.listFiles();
+
+		for (File file : list) {
+			String fn = file.getName();
+			if (fn.startsWith("collected") && fn.endsWith(".csv")) {
+				file.delete();
+			}
+		}
+		
+		System.out.println("Directory cleared of captured financial data.");
+
+	}
+
+	public void run(final boolean collectMode) {
+
+		if (collectMode) {
+			System.out.println("Ready to collect data from remote indicator.");
+		} else {
+			System.out.println("Indicator ready.");
+		}
+
+		System.out.println("Waiting for connections on port " + PORT);
+
+		IndicatorServer server = new IndicatorServer();
+		server.addListener(this);
+
+		server.addIndicatorFactory(new IndicatorFactory() {
+			@Override
+			public String getName() {
+				return "MYIND";
+			}
+
+			@Override
+			public IndicatorListener create() {
+				return new MyInd(collectMode, path);
+			}
+		});
+
+		server.start();
+
+	}
+
+	public static void main(String[] args) {		
+
+		if (args.length != 2) {
+			System.out
+					.println("Usage: IndicatorExample [clear/collect/generate/train/run] [work path]");
+		} else {
+			IndicatorExample program = new IndicatorExample(new File(args[1]));
+			if (args[0].equalsIgnoreCase("collect")) {
+				program.run(true);
+			} else if (args[0].equalsIgnoreCase("train")) {
+				program.train();
+			} else if (args[0].equalsIgnoreCase("run")) {
+				program.run(false);
+			} else if (args[0].equalsIgnoreCase("clear")) {
+				program.clear();
+			} else if (args[0].equalsIgnoreCase("generate")) {
+				program.generate();
+			}
+		}
+	}
+
+	@Override
+	public void notifyConnections(IndicatorLink link, boolean hasOpened) {
+		if (hasOpened) {
+			System.out.println("Connection from " + link.getSocket().toString()
+					+ " established.");
+		} else if (!hasOpened) {
+			System.out.println("Connection from " + link.getSocket().toString()
+					+ " terminated.");
+		}
+	}
+}
---
+++ src/main/java/org/encog/examples/indicator/ninja/avg/MyInd.java
@@ -0,0 +1,114 @@
+package org.encog.examples.indicator.ninja.avg;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.encog.EncogError;
+import org.encog.cloud.indicator.basic.BasicIndicator;
+import org.encog.cloud.indicator.basic.InstrumentHolder;
+import org.encog.cloud.indicator.server.IndicatorPacket;
+
+public class MyInd extends BasicIndicator {
+
+	private InstrumentHolder holder = new InstrumentHolder();
+	private int rowsDownloaded;
+	private boolean collectMode;
+	private File path;
+
+	public MyInd(boolean theCollectMode, File thePath) {
+		super(!theCollectMode);
+		this.collectMode = theCollectMode;
+		this.path = thePath;
+		
+		requestData("CLOSE[1]");
+		requestData("SMA(10)[1]");
+		requestData("SMA(25)[1]");
+	}
+
+	@Override
+	public void notifyPacket(IndicatorPacket packet) {
+		String security = packet.getArgs()[1];
+		long when = Long.parseLong(packet.getArgs()[0]);
+		String key = security.toLowerCase();
+
+		if (this.collectMode) {
+			if (holder.record(when, 2, packet.getArgs())) {
+				this.rowsDownloaded++;
+			}
+		} else {
+
+		}
+	}
+
+	public File nextFile() {
+		int mx = -1;
+		File[] list = this.path.listFiles();
+
+		for (File file : list) {
+			String fn = file.getName();
+			if (fn.startsWith("collected") && fn.endsWith(".csv")) {
+				int idx = fn.indexOf(".csv");
+				String str = fn.substring(9, idx);
+				int n = Integer.parseInt(str);
+				mx = Math.max(n, mx);
+			}
+		}
+
+		return new File(path, "collected" + (mx + 1) + ".csv");
+	}
+
+	public void writeCollectedFile() {
+		File targetFile = nextFile();
+
+		try {
+			FileWriter outFile = new FileWriter(targetFile);
+			PrintWriter out = new PrintWriter(outFile);
+
+			// output header
+			out.print("\"WHEN\"");
+			int index = 0;
+			for (String str : this.getDataRequested()) {
+				String str2;
+				
+				// strip off [ if needed
+				int ix = str.indexOf('[');
+				if (ix != -1) {
+					str2 = str.substring(0, ix).trim();
+				} else {
+					str2 = str;
+				}
+				
+				int c = getDataCount().get(index++);
+				if (c <= 1) {
+					out.print(",\"" + str2 + "\"");
+				} else {
+					for (int i = 0; i < c; i++) {						
+						out.print(",\"" + str2 + "-b" + i + "\"");
+					}
+				}
+			}
+			out.println();
+
+			// output data
+
+			for (Long key : holder.getSorted()) {
+				String str = holder.getData().get(key);
+				out.println(key + "," + str);
+			}
+
+			out.close();
+		} catch (IOException ex) {
+			throw new EncogError(ex);
+		}
+	}
+
+	@Override
+	public void notifyTermination() {
+		if (this.collectMode) {
+			writeCollectedFile();
+		}
+	}
+
+}
