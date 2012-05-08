03e98c31003f844d2739315f016bdc794df9f8b5
Jeff Heaton <jeff@jeffheaton.com>
2012-05-07 20:09:42 -0500

    More work on example indicator

--- src/main/java/org/encog/examples/indicator/ninja/avg/GenerateTraining.java
+++ src/main/java/org/encog/examples/indicator/ninja/avg/GenerateTraining.java
@@ -2,20 +2,39 @@ package org.encog.examples.indicator.ninja.avg;
 
 import java.io.File;
 
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.basic.BasicMLData;
+import org.encog.ml.data.buffer.BufferedMLDataSet;
+import org.encog.util.arrayutil.NormalizedField;
 import org.encog.util.arrayutil.WindowDouble;
 import org.encog.util.csv.CSVFormat;
 import org.encog.util.csv.ReadCSV;
 
 public class GenerateTraining {
 
-	private File path;	
-	private WindowDouble window = new WindowDouble(10);
+	private final File path;	
+	private final File trainingFile;
+	private final WindowDouble window = new WindowDouble(10);
+	private final double pipSize;
+	private NormalizedField fieldDifference;
+	private NormalizedField fieldOutcome;
 	
-	public GenerateTraining(File thePath) {
+	public GenerateTraining(File thePath,double thePipSize) {
 		this.path = thePath;
+		this.pipSize = thePipSize;
+		this.trainingFile = new File(this.path,"training.egb");
+		this.fieldDifference = new NormalizedField();
 	}
 	
-	public void processFile(File file) {
+	public GenerateTraining(File thePath) {
+		this(thePath,0.0001);
+	}
+		
+	public void processFile(File file, BufferedMLDataSet output) {
+		
+		MLData inputData = new BasicMLData(output.getInputSize());
+		MLData idealData = new BasicMLData(output.getIdealSize());
+				
 		ReadCSV csv = new ReadCSV(file.toString(),true,CSVFormat.ENGLISH);
 		while(csv.next()) {
 			double a[] = new double[2];
@@ -26,17 +45,38 @@ public class GenerateTraining {
 			a[0] = close;
 			a[1] = diff;
 			window.add(a);
+			
+			if( window.isFull() ) {
+				double max = (this.window.calculateMax(0,3)-close)/pipSize;
+				double min = (this.window.calculateMin(0,3)-close)/pipSize;
+				double o;
+				
+				if( Math.abs(max)>Math.abs(min) ) {
+					o = max;
+				} else {
+					o = min;
+				}
+				
+				output.add(inputData, idealData);
+			}			
 		}
 	}
 	
 	public void generate() {
 		File[] list = this.path.listFiles();
+		
+		this.trainingFile.delete();
+		BufferedMLDataSet output = new BufferedMLDataSet(this.trainingFile);
+		output.beginLoad(3, 1);
 
 		for (File file : list) {
 			String fn = file.getName();
 			if (fn.startsWith("collected") && fn.endsWith(".csv")) {
-				processFile(file);
+				processFile(file, output);
 			}
 		}
+		
+		output.endLoad();
+		output.close();
 	}
 }
