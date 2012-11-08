ed6d0b2f474a1c086f0dd3756532a1ea81a15277
Jeff Heaton <jeff@jeffheaton.com>
2012-11-07 19:39:30 -0600

    fix compile error

--- src/main/java/org/encog/examples/ml/prg/Benchmark.java
+++ src/main/java/org/encog/examples/ml/prg/Benchmark.java
@@ -1,6 +1,7 @@
 package org.encog.examples.ml.prg;
 
 import org.encog.ml.prg.EncogProgram;
+import org.encog.parse.expression.common.RenderCommonExpression;
 import org.encog.util.Stopwatch;
 
 public class Benchmark {
@@ -11,12 +12,15 @@ public class Benchmark {
 	public static void main(String[] args) {
 		EncogProgram expression = new EncogProgram("((a+25)^3/25)-((a*3)^4/250)");
 		
+		RenderCommonExpression render = new RenderCommonExpression();
+		System.out.println(render.render(expression));
+		
 		
 		Stopwatch sw = new Stopwatch();
 		sw.start();
-		for(double a = 0;a<10000000;a++) {
-			expression.setVariable("a",a);
-			expression.evaluate(0).toFloatValue();
+		for(double a = 0;a<COUNT;a++) {
+			expression.getVariables().setVariable("a",a);
+			expression.evaluate();
 		}
 		sw.stop();
 		
