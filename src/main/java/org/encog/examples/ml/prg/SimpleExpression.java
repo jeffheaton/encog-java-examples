9d6148881b8cac7b15018ebd4436288037b36e99
Jeff Heaton <jeff@jeffheaton.com>
2012-11-12 07:03:52 -0600

    Beginning of GP example

--- src/main/java/org/encog/examples/ml/prg/SimpleExpression.java
+++ src/main/java/org/encog/examples/ml/prg/SimpleExpression.java
@@ -2,6 +2,10 @@ package org.encog.examples.ml.prg;
 
 import org.encog.mathutil.EncogFunction;
 import org.encog.ml.data.MLDataSet;
+import org.encog.ml.prg.EncogProgramContext;
+import org.encog.ml.prg.extension.StandardExtensions;
+import org.encog.ml.prg.train.PrgPopulation;
+import org.encog.ml.prg.train.rewrite.RewriteConstants;
 import org.encog.util.data.GenerationUtil;
 
 public class SimpleExpression {
@@ -21,7 +25,17 @@ public class SimpleExpression {
 			
 		}, 0, 100, 1);
 		
-
+		EncogProgramContext context = new EncogProgramContext();
+		context.defineVariable("x");
 		
+		StandardExtensions.createNumericOperators(context.getFunctions());
+		
+		PrgPopulation pop = new PrgPopulation(context,trainingData,1000);
+		pop.addRewriteRule(new RewriteConstants());
+		pop.createRandomPopulation(5);
+		pop.sort();
+		
+		//System.out.println(train.getError());
+		pop.dumpMembers();
 	}
 }
