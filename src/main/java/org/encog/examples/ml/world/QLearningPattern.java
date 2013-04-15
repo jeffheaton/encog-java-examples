18e1007f7a126e2806278e7ee78575a221a5245a
Jeff Heaton <jheaton@heatonresearch.com>
2013-04-15 17:52:26 -0500

    Updated headers for 2013 and 3.2

--- src/main/java/org/encog/examples/ml/world/QLearningPattern.java
+++ src/main/java/org/encog/examples/ml/world/QLearningPattern.java
@@ -1,9 +1,9 @@
 /*
  * Encog(tm) Java Examples v3.2
  * http://www.heatonresearch.com/encog/
- * http://code.google.com/p/encog-java/
- 
- * Copyright 2008-2012 Heaton Research, Inc.
+ * https://github.com/encog/encog-java-examples
+ *
+ * Copyright 2008-2013 Heaton Research, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
