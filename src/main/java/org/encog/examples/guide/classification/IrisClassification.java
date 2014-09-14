01df78d4d813f9776ac7a33c27279f8e50612abc
Jeff Heaton <jeff@jeffheaton.com>
2014-09-14 08:36:59 -0500

    Iris classification example

--- src/main/java/org/encog/examples/guide/classification/IrisClassification.java
+++ src/main/java/org/encog/examples/guide/classification/IrisClassification.java
@@ -7,18 +7,17 @@ import java.util.Arrays;
 
 import org.encog.Encog;
 import org.encog.bot.BotUtil;
+import org.encog.ml.MLRegression;
 import org.encog.ml.data.MLData;
-import org.encog.ml.data.versatile.BasicNormalizationStrategy;
 import org.encog.ml.data.versatile.CSVDataSource;
 import org.encog.ml.data.versatile.ColumnDefinition;
 import org.encog.ml.data.versatile.ColumnType;
 import org.encog.ml.data.versatile.NormalizationHelper;
 import org.encog.ml.data.versatile.VersatileDataSource;
 import org.encog.ml.data.versatile.VersatileMLDataSet;
-import org.encog.neural.networks.BasicNetwork;
+import org.encog.ml.model.EncogModel;
 import org.encog.util.csv.CSVFormat;
 import org.encog.util.csv.ReadCSV;
-import org.encog.util.simple.EncogUtility;
 
 public class IrisClassification {
 	public static String DATA_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data";
@@ -40,27 +39,70 @@ public class IrisClassification {
 
 	public void run(String[] args) {
 		try {
+			// Download the data that we will attempt to model.
 			File irisFile = downloadData(args);
+			
+			// Define the format of the data file.
+			// This area will change, depending on the columns and 
+			// format of the file that you are trying to model.
 			VersatileDataSource source = new CSVDataSource(irisFile, false,
 					CSVFormat.DECIMAL_POINT);
-			VersatileMLDataSet data = new VersatileMLDataSet(source,
-					new BasicNormalizationStrategy(-1, 1, -1, 1));
+			VersatileMLDataSet data = new VersatileMLDataSet(source);
 			data.defineSourceColumn("sepal-length", ColumnType.continuous);
 			data.defineSourceColumn("sepal-width", ColumnType.continuous);
 			data.defineSourceColumn("petal-length", ColumnType.continuous);
 			data.defineSourceColumn("petal-width", ColumnType.continuous);
+			
+			// Define the column that we are trying to predict.
 			ColumnDefinition outputColumn = data.defineSourceColumn("species",
 					ColumnType.nominal);
-			data.analyze();
-			data.normalizeSingleOutputOthersInput(outputColumn);
-
-			BasicNetwork network = EncogUtility.simpleFeedForward(data.getInputSize(), 5, 0, data.getIdealSize(), true);
-			EncogUtility.trainToError(network, data, 0.05);
 			
+			// Analyze the data, determine the min/max/mean/sd of every column.
+			data.analyze();
+			
+			// Map the prediction column to the output of the model, and all
+			// other columns to the input.
+			data.defineSingleOutputOthersInput(outputColumn);
+			
+			// Create feedforward neural network as the model type. MLMethodFactory.TYPE_FEEDFORWARD.
+			// You could also other model types, such as:
+			// MLMethodFactory.SVM:  Support Vector Machine (SVM)
+			// MLMethodFactory.TYPE_RBFNETWORK: RBF Neural Network
+			// MLMethodFactor.TYPE_NEAT: NEAT Neural Network
+			// MLMethodFactor.TYPE_PNN: Probabilistic Neural Network
+			EncogModel model = new EncogModel(data);
+			
+			// Now normalize the data.  Encog will automatically determine the correct normalization
+			// type based on the model you chose in the last step.
+			data.normalize();
+			
+			// Hold back some data for a final validation.
+			// Shuffle the data into a random ordering.
+			// Use a seed of 1001 so that we always use the same holdback and will get more consistent results.
+			model.holdBackValidation(0.3, true, 1001);
+			
+			// Choose whatever is the default training type for this model.
+			model.selectTrainingType(data);
+			
+			// Use a 5-fold cross-validated train.  Return the best method found.
+			MLRegression bestMethod = (MLRegression)model.crossvalidate(5, true);
+
+			// Display the training and validation errors.
+			System.out.println( "Training error: " + model.calculateError(bestMethod, model.getTrainingDataset()));
+			System.out.println( "Validation error: " + model.calculateError(bestMethod, model.getValidationDataset()));
+			
+			// Display our normalization parameters.
 			NormalizationHelper helper = data.getNormHelper();
 			System.out.println(helper.toString());
 			
+			// Display the final model.
+			System.out.println("Final model: " + bestMethod);
 			
+			// Loop over the entire, original, dataset and feed it through the model.
+			// This also shows how you would process new data, that was not part of your
+			// training set.  You do not need to retrain, simply use the NormalizationHelper
+			// class.  After you train, you can save the NormalizationHelper to later
+			// normalize and denormalize your data.
 			ReadCSV csv = new ReadCSV(irisFile, false, CSVFormat.DECIMAL_POINT);
 			String[] line = new String[4];
 			MLData input = helper.allocateInputVector();
@@ -73,7 +115,7 @@ public class IrisClassification {
 				line[3] = csv.get(3);
 				String correct = csv.get(4);
 				helper.normalizeInputVector(line,input);
-				MLData output = network.compute(input);
+				MLData output = bestMethod.compute(input);
 				String irisChosen = helper.denormalizeOutputVectorToString(output)[0];
 				
 				result.append(Arrays.toString(line));
@@ -86,7 +128,7 @@ public class IrisClassification {
 				System.out.println(result.toString());
 			}
 			
-			
+			irisFile.delete();
 			Encog.getInstance().shutdown();
 
 		} catch (Exception ex) {
