c4372d20f9b62a111c78d068ca2b3926b829ad1a
Jeff Heaton <jeff@jeffheaton.com>
2017-08-29 20:19:55 -0500

    Update copyright on examples.

--- src/main/java/org/encog/examples/ml/tsp/City.java
+++ src/main/java/org/encog/examples/ml/tsp/City.java
@@ -3,7 +3,7 @@
  * http://www.heatonresearch.com/encog/
  * https://github.com/encog/encog-java-examples
  *
- * Copyright 2008-2016 Heaton Research, Inc.
+ * Copyright 2008-2017 Heaton Research, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
